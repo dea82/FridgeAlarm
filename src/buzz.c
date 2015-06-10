d9d42f5d51a1563044b819a2c3591d64c7a4d426
Andreas <andreas.laghamn@gmail.com>
2015-06-10 10:38:36 +0200

    Made a huge clean of unnecessary files.

--- src/buzz.c
+++ src/buzz.c
@@ -15,17 +15,47 @@ static tSoundType_E soundType_E;
 void Buzz_init(void)
 {
     CONF_IO(BUZZ_CFG, OUTPUT, 0);
+
+#if 1
+
+    TCCR0B = (1 << CS01) | (1 << CS00);
+    // Set to 'Fast PWM' mode
+    TCCR0A |= (1 << WGM01) | (1 << WGM00) | (1 << COM0B1);
+
+    OCR0B = 15;
+#endif
+
 }
 
 void Buzz_loop(void)
 {
+    static tU16 counter_U16;
+
     switch (soundType_E)
     {
     case BUZZ_OFF_E:
         IO_CLR(BUZZ_CFG);
         break;
-    default:
+    case BUZZ_ON_E:
         IO_SET(BUZZ_CFG);
+        break;
+    case BUZZ_ALARM_E:
+        if (counter_U16 > 1000)
+        {
+            counter_U16 = 0;
+        }
+        else
+        {
+            counter_U16++;
+        }
+        if (counter_U16 < 500)
+        {
+            IO_SET(BUZZ_CFG);
+        }
+        else
+        {
+            IO_CLR(BUZZ_CFG);
+        }
         break;
     }
 }
