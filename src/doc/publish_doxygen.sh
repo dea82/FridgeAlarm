4bc95971e87690232a8dee100d8480dbad07e319
Andreas Laghamn <andreas.laghamn@gmail.com>
2016-06-16 11:39:59 +0200

    Rewritten initialize of new doc branch

--- src/doc/publish_doxygen.sh
+++ src/doc/publish_doxygen.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e #Exit with nonzero exit code if anything fails
 
+# Commits to this branch will be deployed
 SOURCE_BRANCH="develop"
+# Deployed to this branch
 TARGET_BRANCH="gh-pages"
-
+# The deployment working directory
+CLONE_DIR="gh-pages"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Don't deploy on other branches than develop."
+    echo "Don't  on other branches than develop."
     exit 0;
 fi
 
@@ -15,39 +18,40 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-rm -rf gh-repo-temp
-git clone $REPO gh-repo-temp
-cd gh-repo-temp
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-git rm -rf .
-# Helps us get rid of hidden files.
-git clean -fxd
-cd ..
-make doc
-cd gh-repo-temp
-cp -r ../doc/doxygen/* .
-git reset .
+# Get exsisting documentation
+git clone $REPO $CLONE_DIR
+# If no doc branch is available create one (orphan)
+git -C $CLONE_DIR checkout $TARGET_BRANCH || git -C $CLONE_DIR checkout --orphan $TARGET_BRANCH
+# This will reset a fresh orphan branch to include nothing!
+git -C $CLONE_DIR reset --hard
+# This will remove all old files in the repo. --ignore-unmatch
+# is necessary for the first time when the branch is created.
+git -C $CLONE_DIR rm -rf --ignore-unmatch .
+pwd
+make -C . doc
+# Copy all generated documents to the repo location
+cp -r doc/doxygen/* $CLONE_DIR
 
-git config user.name "Travis CI"
-git config user.email "andreas.laghamn@gmail.com"
+git -C $CLONE_DIR config user.name "Travis CI"
+git -C $CLONE_DIR config user.email "andreas.laghamn@gmail.com"
 
-if [[ -z $(git status --porcelain) ]]; then
+if [[ -z $(git -C $CLONE_DIR status --porcelain) ]]; then
     echo "No changes to documentation, do not deploy!"
     exit 0
 fi
-	
-git add .
-git commit -m "Deploy to GitHub Pages ${SHA}"
+
+git -C $CLONE_DIR add .
+git -C $CLONE_DIR commit -m "Deploy to GitHub Pages ${SHA}"
 
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../doc/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in doc/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
-git push $SSH_REPO $TARGET_BRANCH
-cd ..
-rm -rf gh-repo-temp
+git -C $CLONE_DIR push $SSH_REPO $TARGET_BRANCH
+
+echo "Successfully deployed Doxygen documentation!"
