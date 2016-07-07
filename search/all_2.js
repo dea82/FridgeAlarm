3a571e05133de467bbfe0a6738c421479ab960c7
Travis CI <andreas.laghamn@gmail.com>
2016-07-07 20:10:12 +0000

    Travis: Deploy to GitHub Pages c7050e72d711278558b85b3df86ea34065a588ae

--- search/all_2.js
+++ search/all_2.js
@@ -11,9 +11,9 @@ var searchData=
   ['cont_5floop',['Cont_loop',['../cont_8c.html#a79511b0620c51a6ea1418cf2b0daba0e',1,'Cont_loop(void):&#160;cont.c'],['../cont_8h.html#a79511b0620c51a6ea1418cf2b0daba0e',1,'Cont_loop(void):&#160;cont.c']]],
   ['cpul_2ec',['cpul.c',['../cpul_8c.html',1,'']]],
   ['cpul_2eh',['cpul.h',['../cpul_8h.html',1,'']]],
-  ['cpul_5fcreateresultblock_5fstr',['Cpul_CreateResultBlock_str',['../cpul_8c.html#a24257c66d0017ac73b8f756c5a941993',1,'cpul.c']]],
-  ['cpul_5fstartpoint',['Cpul_startPoint',['../cpul_8h.html#a0c278eda3299ef03023e00be9ca9bc92',1,'cpul.h']]],
+  ['cpul_5fcreateresultblock_5fstr',['Cpul_CreateResultBlock_str',['../cpul_8c.html#a24257c66d0017ac73b8f756c5a941993',1,'Cpul_CreateResultBlock_str(char name_c[], tU08 cycles_U08, tU08 prescaler_U08):&#160;cpul.c'],['../cpul_8h.html#a24257c66d0017ac73b8f756c5a941993',1,'Cpul_CreateResultBlock_str(char name_c[], tU08 cycles_U08, tU08 prescaler_U08):&#160;cpul.c']]],
+  ['cpul_5fstartpoint',['Cpul_startPoint',['../cpul_8h.html#afa9856232c21c38a106e275195492a6f',1,'cpul.h']]],
   ['cpul_5fstoppoint_5fu08',['Cpul_stopPoint_U08',['../cpul_8h.html#a67da29d63e4520043093762151a7dfaa',1,'cpul.h']]],
-  ['crc_5fu16',['crc_U16',['../structtCpul__ResultBlock__str.html#a5fabcc376ade40af85a3149f41469cee',1,'tCpul_ResultBlock_str']]],
+  ['crc_5fu08',['crc_U08',['../structtCpul__ResultBlock__str.html#a9a9c57e1493e317c49c038ffa732a983',1,'tCpul_ResultBlock_str']]],
   ['cycles_5fu08',['cycles_U08',['../structtCpul__ResultBlock__str.html#a029b59aa30675f2248abb48dccdd12e3',1,'tCpul_ResultBlock_str']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['crc_5fu16',['crc_U16',['../structtCpul__ResultBlock__str.html#a5fabcc376ade40af85a3149f41469cee',1,'tCpul_ResultBlock_str']]],
+  ['crc_5fu08',['crc_U08',['../structtCpul__ResultBlock__str.html#a9a9c57e1493e317c49c038ffa732a983',1,'tCpul_ResultBlock_str']]],
   ['cycles_5fu08',['cycles_U08',['../structtCpul__ResultBlock__str.html#a029b59aa30675f2248abb48dccdd12e3',1,'tCpul_ResultBlock_str']]]
 ];
