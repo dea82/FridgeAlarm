d2bf3387ff7a1aadd2233bc02c1cab504e047386
Andreas Laghamn <andreas.laghamn@gmail.com>
2016-08-25 14:40:56 +0200

    Updated piezo-test file

--- src/tools/piezo-tester/main.c
+++ src/tools/piezo-tester/main.c
@@ -1,11 +1,29 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 /* Declaring main as OS_main saves some register pushing to stack. */
 int main(void) __attribute__((OS_main));
 int main(void)
 {
+    
+#if F_CPU == 9600000
+    OCR0A = 43;
+#else
+#error "Correct compare value to CPU speed."
+#endif
+
+DDRB = _BV(PB0);
+
     for(;;)
     {
-        PORTB = 1;
+        /* Set to 'CTC' mode, toggle on match */
+        TCCR0A = _BV(COM0A0) | _BV(WGM01);
+        /* Start clock - prescaler 64 */
+        TCCR0B = _BV(CS01) | _BV(CS00);
+        _delay_ms(1000);
+
+        TCCR0B = 0;
+        TCCR0A = 0;
+        _delay_ms(1000);
     }
 }
