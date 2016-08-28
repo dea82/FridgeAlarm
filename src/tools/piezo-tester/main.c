5fb30ce0d73c75cf52f5495fd5a0f90c23b7c185
Andreas Laghamn <andreas.laghamn@gmail.com>
2016-08-28 21:53:00 +0200

    Updated schematics for "production" version and buzzer proto file

--- src/tools/piezo-tester/main.c
+++ src/tools/piezo-tester/main.c
@@ -5,22 +5,23 @@
 int main(void) __attribute__((OS_main));
 int main(void)
 {
-    
+
 #if F_CPU == 9600000
-    OCR0A = 43;
+    OCR0A = 86;
+    OCR0B = 43;
 #else
 #error "Correct compare value to CPU speed."
 #endif
 
-DDRB = _BV(PB0);
+DDRB = _BV(PB1);
 
     for(;;)
     {
         /* Set to 'CTC' mode, toggle on match */
-        TCCR0A = _BV(COM0A0) | _BV(WGM01);
+        TCCR0A = _BV(COM0B1) | _BV(WGM01) | _BV(WGM00);
         /* Start clock - prescaler 64 */
-        TCCR0B = _BV(CS01) | _BV(CS00);
-        _delay_ms(1000);
+        TCCR0B = _BV(WGM01) | _BV(CS01) | _BV(CS00);
+        _delay_ms(3000);
 
         TCCR0B = 0;
         TCCR0A = 0;
