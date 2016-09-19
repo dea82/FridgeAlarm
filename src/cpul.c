5d4b8cef2bfab3c2304148a2dd6162678db581e1
Andreas Laghamn <andreas.laghamn@gmail.com>
2016-09-19 20:50:38 +0200

    Implement own version of crc8
    Travis has a pretty old avr-libc without support for CRC8. Instead fighting with updates of Travis now cpul includes it's own CRC8 calculation.

--- src/cpul.c
+++ src/cpul.c
@@ -1,7 +1,32 @@
-#include <util/crc16.h>
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015-2016 Andreas L.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include <avr/pgmspace.h>
 
 #include "cpul.h"
+
+static tU08 crc8_U08 (tU08 inCrc, tU08 inData);
 
 tCpul_ResultBlock_str Cpul_CreateResultBlock_str(const char * name_c, const tU08 cycles_U08, const tU08 prescaler_U08)
 {
@@ -15,8 +40,30 @@ tCpul_ResultBlock_str Cpul_CreateResultBlock_str(const char * name_c, const tU08
 
   for (tU08 byte_U08 = sizeof(tCpul_ResultBlock_str); byte_U08 > sizeof(((tCpul_ResultBlock_str*)0)->crc_U08); byte_U08--)
   {
-    resultBlock_str.crc_U08 = _crc8_ccitt_update(resultBlock_str.crc_U08, *data_pU08++);
+    resultBlock_str.crc_U08 = crc8_U08(resultBlock_str.crc_U08, *data_pU08++);
   }
 
   return resultBlock_str;
+}
+
+static tU08 crc8_U08 (tU08 inCrc_U08, tU08 inData_U08)
+{
+    tU08 i_U08;
+    tU08 data_U08;
+
+    data_U08 = inCrc_U08 ^ inData_U08;
+
+    for ( i_U08 = 0; i_U08 < 8; i_U08++ )
+    {
+        if (( data_U08 & 0x80 ) != 0 )
+        {
+            data_U08 <<= 1;
+            data_U08 ^= 0x07;
+        }
+        else
+        {
+            data_U08 <<= 1;
+        }
+    }
+    return data_U08;
 }
